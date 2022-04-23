0d5ea5d5dc4a28a535290cda7da3448f854e5b10
Ajay Aravindh R <94431070+Dark-knight-222@users.noreply.github.com>
2022-04-23 12:33:03 +0530

    Add files via upload

---
+++ Text.java
@@ -0,0 +1,10 @@
+package com.string;
+
+public class Text {
+    public static void main(String[] args) {
+        String name = "Ajay" + " Aravindh" + " R";
+        int len = name.length();
+        System.out.println(name);
+        System.out.println(len);
+    }
+}
